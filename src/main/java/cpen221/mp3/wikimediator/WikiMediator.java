b38ee8ddfef7798494e067dfd6b83dd477ba064c
John Baek <johnbaek5059@gmail.com>
2021-11-28 21:21:50 -0800

    wikimediator skel

--- src/main/java/cpen221/mp3/wikimediator/WikiMediator.java
+++ src/main/java/cpen221/mp3/wikimediator/WikiMediator.java
@@ -1,5 +1,8 @@
 package cpen221.mp3.wikimediator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WikiMediator {
 
     /* TODO: Implement this datatype
@@ -13,5 +16,33 @@ public class WikiMediator {
         values like null.
 
      */
+
+    public WikiMediator(int capacity, int stalenessInterval) {
+
+    }
+
+    public List<String> search(String query, int limit) {
+        return new ArrayList<>();
+    }
+
+    public String getPage(String pageTitle) {
+        return "abc";
+    }
+
+    public List<String> zeitgeist(int limit) {
+        return new ArrayList<>();
+    }
+
+    public List<String> trending(int timeLimitInSeconds, int maxItems) {
+        return new ArrayList<>();
+    }
+
+    public int windowedPeakLoad(int timeWindowInSeconds) {
+        return 0;
+    }
+
+    public int windowedPeakLoad() {
+        return 0;
+    }
 
 }
