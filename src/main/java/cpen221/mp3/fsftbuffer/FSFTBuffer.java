4ec52c72bc175a0fb8491d07dcf82a1498e80d5b
John Baek <johnbaek5059@gmail.com>
2021-11-28 21:05:14 -0800

    ...

--- src/main/java/cpen221/mp3/fsftbuffer/FSFTBuffer.java
+++ src/main/java/cpen221/mp3/fsftbuffer/FSFTBuffer.java
@@ -24,6 +24,7 @@ public class FSFTBuffer<T extends Bufferable> {
     // objectTimeRecord is the map that the key is the ID of bufferable object in buffer and the values represents the inserted time
 
     public static final int ONE_SEC = 1000; // 1000 is millisecond
+
     /* the default buffer size is 32 objects */
     public static final int DSIZE = 32;
 
