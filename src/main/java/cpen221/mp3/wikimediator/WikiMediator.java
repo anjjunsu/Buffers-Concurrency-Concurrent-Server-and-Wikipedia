aa7bda1e87801b933a4a91a9d934941330ee9f76
John Baek <johnbaek5059@gmail.com>
2021-12-03 16:29:20 -0800

    search and getPage draft

--- src/main/java/cpen221/mp3/wikimediator/WikiMediator.java
+++ src/main/java/cpen221/mp3/wikimediator/WikiMediator.java
@@ -1,6 +1,7 @@
 package cpen221.mp3.wikimediator;
 
 import cpen221.mp3.fsftbuffer.FSFTBuffer;
+import cpen221.mp3.fsftbuffer.ObjectDoesNotExistException;
 import org.fastily.jwiki.core.Wiki;
 
 import java.util.ArrayList;
@@ -18,18 +19,19 @@ public class WikiMediator {
     // fill this out
 
 
-    private FSFTBuffer cache;
+    private FSFTBuffer<Page> cache;
     private Wiki wiki;
+    private Page pageObject;
 
     /**
      * Create a WikiMediator cache with capacity and stalenessInterval
      *
-     * @param capacity           the number of pages to be cached
-     * @param stalenessInterval  the number of seconds after which a page in the cache
-     *                           will become stale
+     * @param capacity          the number of pages to be cached
+     * @param stalenessInterval the number of seconds after which a page in the cache
+     *                          will become stale
      */
     public WikiMediator(int capacity, int stalenessInterval) {
-        cache = new FSFTBuffer<Page>(capacity, stalenessInterval);
+        cache = new FSFTBuffer<>(capacity, stalenessInterval);
         wiki = new Wiki.Builder().withDomain("en.wikipedia.org").build();
     }
 
@@ -37,18 +39,46 @@ public class WikiMediator {
      * Given a query, return up to limit page titles that match the query string (per Wikipedia's
      * search service).
      *
-     * @param query     page title requested
-     * @param limit     number of page titles to be returned
-     * @return          list of page titles from query up to limit page titles.
+     * @param query page title requested
+     * @param limit number of page titles to be returned
+     * @return      list of page titles from query up to limit page titles.
      */
 
     public List<String> search(String query, int limit) {
+        try {
+            pageObject = cache.get(query); //qeury needs to be changed to id
+        } catch (ObjectDoesNotExistException e) {
+            pageObject = new Page(query, getPage(query), 1); //id will be changed later
+            boolean cachePut = cache.put(pageObject);
+        }
+
         return new ArrayList<>(wiki.search(query, limit));
     }
 
+    /**
+     * Return the context of given pageTitle.
+     *
+     * @param pageTitle page title of the content
+     * @return content with the given page title
+     */
+
     public String getPage(String pageTitle) {
-        return wiki.getPageText(pageTitle);
+        try {
+            pageObject = cache.get(pageTitle); // pageTitle needs to be changed to id
+        } catch(ObjectDoesNotExistException e) {
+            pageObject = new Page(pageTitle, getPage(pageTitle), 1); //id will be changed later
+            boolean cachePut = cache.put(pageObject);
+        }
+        return pageObject.content();
     }
+
+    /**
+     * Return the most common strings used in search and getPage requests, with items being sorted
+     * in non-increasing count order up to limit number of items.
+     *
+     * @param limit maximum number of items to be returned
+     * @return list of strings that are used in search and getPage requests the most.
+     */
 
     public List<String> zeitgeist(int limit) {
         return new ArrayList<>();
