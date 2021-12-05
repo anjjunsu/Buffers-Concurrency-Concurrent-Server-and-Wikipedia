e1eb659a30469d67bf0a0e4e1a06e50b9afa3daa
John Baek <johnbaek5059@gmail.com>
2021-12-04 18:46:59 -0800

    Task3 search, getPage, zeitgeist

--- src/test/java/cpen221/mp3/Task3_Test.java
+++ src/test/java/cpen221/mp3/Task3_Test.java
@@ -1,2 +1,47 @@
-package cpen221.mp3;public class Task3_Test {
+package cpen221.mp3;
+
+import cpen221.mp3.wikimediator.WikiMediator;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task3_Test {
+    public static final int FIVE_CAPACITY = 5;
+    public static final int TEN_CAPACITY = 10;
+    public static final int FIVE_SEC_TO_LIVE = 5;
+    public static final int TEN_SEC_TO_LIVE = 10;
+    public static final int ONE_SEC = 1000;
+
+    @Test
+    public void testSearch() {
+        WikiMediator wikime = new WikiMediator(FIVE_CAPACITY, FIVE_SEC_TO_LIVE);
+        System.out.println(wikime.search("Barack Obama", 5));
+        System.out.println(wikime.search("Hello", 10));
+    }
+
+    @Test
+    public void testGetPageAndZeitgeist() {
+        WikiMediator wikime = new WikiMediator(FIVE_CAPACITY, FIVE_SEC_TO_LIVE);
+        System.out.println(wikime.getPage("Hello"));
+        wikime.getPage("Barack Obama");
+        System.out.println(wikime.getPage("Barack Obama"));
+        wikime.getPage("Hello");
+        wikime.getPage("Hello");
+        System.out.println(wikime.getPage("abc"));
+        wikime.getPage("abc");
+        wikime.getPage("abc");
+        wikime.getPage("abc");
+
+        List<String> zeitgeistList = new ArrayList<>();
+        zeitgeistList.add("abc");
+        zeitgeistList.add("Hello");
+        zeitgeistList.add("Barack Obama");
+
+
+        Assert.assertEquals(zeitgeistList, wikime.zeitgeist(3));
+
+    }
+
 }
